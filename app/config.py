17a7f6e65d42f28d13420332e147492ca9305ef2
0x6561 <0x6561@gmail.com>
2020-01-07 14:47:46 -0700

    adjusted config.py

--- app/config.py
+++ app/config.py
@@ -7,21 +7,19 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SUBJECT_PREFIX = os.environ.get('MAIL_SUBJECT_PREFIX') or 'resume.0x6561.net Resume Creator '
-    MAIL_SENDER = os.environ.get('MAIL_SENDER') or 'resume.0x6561.net'
+    MAIL_SENDER = os.environ.get('MAIL_SENDER') or 'resume-admin@0x6561.net'
     MAIL_DEBUG=True 
     DEBUG=True 
     MAIL_SUPPRESS_SEND=False
     TESTING = False
-    MAIL_SERVER = 'smtp.googlemail.com'
-    #MAIL_SERVER = 'resume.0x6561.net'
-    MAIL_PORT = 587 
-    #MAIL_PORT=465 
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_PORT= os.environ.get('MAIL_PORT') or 587
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False 
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or True 
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     APP_ADMIN = os.environ.get('APP_ADMIN')
-    MAIL_SUBJECT_PREFIX = 'prefix'
+    MAIL_SUBJECT_PREFIX = 'resume.0x6561.net Resume Creator '
 
     @staticmethod
     def init_app(app):
